99603b3e3f6392cc0cd1183284500ce079519442
sumns <sks23info@gmail.com>
2023-03-14 22:13:41 +0530

    Adding React Day6 solution TodoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, {useState} from "react";
+
+function TodoList(){
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [count, setCount] = useState(0);
+
+  const countTask = ()=>{
+    setCount(count+1)
+  }
+
+  const decreaseTask = () => {
+    if(count > 0){
+      setCount(count-1)
+    }
+  }
+
+  const handleBoth1 = (index)=> {
+    decreaseTask();
+    handleComplete(index)
+  }
+
+  const handleBoth2 = (index) =>{
+    decreaseTask();
+    handleDelete(index)
+
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTasks([...tasks, {text: newTask, completed: false}])
+    setNewTask('')
+  }
+
+  const handleComplete = index => {
+    const newTasks = [...tasks]
+    newTasks[index].completed = !newTasks[index].completed
+    setTasks(newTasks)
+  }
+
+  const handleDelete = index => {
+    const newTasks = [...tasks]
+    newTasks.splice(index, 1)
+    setTasks(newTasks)
+  }
+
+
+  return(
+
+    <div>
+      <h1>Pending Tasks({count})</h1>
+      <form onSubmit = {handleSubmit} >
+        <input
+        type="text"
+        value={newTask}
+        onChange = {e => setNewTask(e.target.value)}
+        />
+
+        <button type="submit" onClick={countTask}>Add Task</button>
+
+      </form>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li
+            key = {index}
+            style = {{textDecoration: task.completed ? 'line-through' : 'none'}} 
+          >
+
+            {task.text}
+            <button onClick = {() => handleBoth1(index)}>complete</button>
+            <button onClick = {() => handleBoth2(index)}>Delete</button>
+          </li>
+        ) )}
+      </ul>
+
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default TodoList
