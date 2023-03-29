87580aa3bec27debb5c85477ff54bcf5f524816b
sumns <sks23info@gmail.com>
2023-03-29 13:50:07 +0530

    i am adding quiz data and css

---
+++ src/Data/QuizData.js
@@ -0,0 +1,28 @@
+export const QuizData = [
+    {
+        question : "JavaScript File Has An Extension of:",
+        options : [".Java", ".js", ".javascript", ".xml"],
+        answer : 2
+
+    },
+    {
+        question : "Which statement cannot be used to declare a variable in JavaScript?",
+        options : ["var", "let", "int", "const"],
+        answer : 3
+    },
+    {
+        question : "Which of the following is block scoped?",
+        options : ["var", "let", "const", "Both B and C"],
+        answer : 4
+    },
+    {
+        question : "Choose the correct HTML tag for the largest heading",
+        options : ["Head", "h1", "h6", "header"],
+        answer : 3
+    },
+    {
+        question : "Choose the correct HTML tag to make a text italic",
+        options : ["italic", "i", "e", "it"],
+        answer : 2
+    }
+]
